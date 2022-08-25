75754905dfb538b0bd1d7c45c6a69fcb1d6060e9
Carson Holland <68627241+Cnh1116@users.noreply.github.com>
2022-08-25 11:26:04 -0500

    Add files via upload

---
+++ Greetings.java
@@ -0,0 +1,5 @@
+public class Greetings {
+    public void English() {System.out.println("Hello!");}
+    public void Spanish() {System.out.println("Hola!");}
+    public void Japanese() {System.out.println("Konichiwa!");}
+}
---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main
+{  
+    public static void main(String args[])
+    {  
+     GreetingsClass(); 
+    }  
+
+    public static void GreetingsClass()
+    {
+        Greetings object = new Greetings();
+        object.English();
+        object.Spanish();
+        object.Japanese();
+    }
+}
